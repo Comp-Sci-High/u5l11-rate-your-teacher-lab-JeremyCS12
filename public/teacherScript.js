24ccbcf3e2eaa9df6d95c3e4048e12a4725214c0
JeremyCS12 <jeremy.ortiz26@compscihigh.org>
2026-04-15 16:41:14 +0000

    lab

---
+++ public/teacherScript.js
@@ -0,0 +1,21 @@
+// Add an event listener to the form that upon submits creates a new teacher
+// When the teacher is created redirect to the teachers page
+
+const teacherForm = document.querySelector("#teacherForm")
+
+teacherForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+
+    const formData = new FormData(teacherForm)
+    const reqBody = Object.fromEntries(formData)
+
+    await fetch("/add/teacher", {
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(reqBody)
+    })
+    window.location.href = "/"
+})
